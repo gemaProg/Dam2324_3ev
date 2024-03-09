adb36f105f9f4928d1fc05e72395bc80a3382171
gemaProg <gema.garciafernandez@educa.madrid.org>
2024-03-09 20:35:32 +0100

    3ev

--- Excepciones/src/main/java/org/example/Main.java
+++ Excepciones/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int lista[] = new int [10];
         System.out.println(lista[lista.length]);
 
