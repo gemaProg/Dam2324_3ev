10cd42a9dff175146866a7f2231f31060a17c847
gemaProg <gema.garciafernandez@educa.madrid.org>
2024-03-09 20:49:15 +0100

    Comienzo 3ev

--- GenerandoDatosAleatorios/src/main/java/org/example/Main.java
+++ GenerandoDatosAleatorios/src/main/java/org/example/Main.java
@@ -1,8 +1,27 @@
 package org.example;
 
+import net.datafaker.Faker;
+
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Faker().);
+        System.out.println("------------------Baloncesto---------------------------");
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println(new Faker().basketball().positions());
+        }
+        System.out.println("----------------------POKEMON--------------------------");
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println(new Faker().pokemon().name());
+        }
+        System.out.println("---------------------HARRY POTTER----------------------");
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println(new Faker().harryPotter().book());
+        }
+        System.out.println("---------------Cómo entrenar a tu dragon------------------------------");
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println(new Faker().howToTrainYourDragon().characters());
+        }
     }
 
 }
