fb8eed80fb729c0adaffd0c3f36b318f3113276e
gemaProg <gema.garciafernandez@educa.madrid.org>
2024-03-09 20:31:54 +0100

    3ev

--- Excepciones/src/main/java/org/example/Main.java
+++ Excepciones/src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int lista[] = new int [10];
+        System.out.println(lista[lista.length]);
+
+        Scanner lector = new Scanner(System.in);
+        System.out.println(lector.nextInt());
+
+        char letra = (char)System.in.read();
+
     }
 }
