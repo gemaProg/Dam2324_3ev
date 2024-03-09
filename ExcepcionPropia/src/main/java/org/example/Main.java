458bc7d3a13da52a05be400a260b378290e5ee7c
gemaProg <gema.garciafernandez@educa.madrid.org>
2024-03-09 20:56:29 +0100

    3ev

---
+++ ExcepcionPropia/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //gestionar mediante una excepcion propia que la opción introducida para un menú esté entre 1 y 10
+
+    }
+}
