10a4456f86e3bd4092580e6808a4acb7cef50c70
Gema Garcia <gema.garciafernandez@educa.madrid.org>
2024-03-12 11:13:24 +0100

    3ev arrayList

---
+++ ArrayListEjemplo/src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import net.datafaker.Faker;
+
+import java.util.ArrayList;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    /**
+     * argumentos de entrada
+     * @param args
+     */
+    public static void main(String[] args) {
+        ArrayList<Integer> lista = new ArrayList();
+        lista.add(2);
+        lista.add(10);
+        for (int i = 0; i < 10; i++) {
+            lista.add(i);
+        }
+        System.out.println(lista.size());
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.print(lista.get(i) + " ");
+        }
+
+        ArrayList<String> listaNombres = new ArrayList();
+        listaNombres.add("Pepe");
+        listaNombres.add("Juan");
+        for (int i = 0; i < 10; i++) {
+            listaNombres.add(new Faker().chiquito().jokes());
+        }
+        System.out.println(lista.size());
+        System.out.println(listaNombres);
+
+        ArrayList<Palabra> listaPalabras = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            listaPalabras.add(new Palabra());
+        }
+        String adivinar = listaPalabras.get((int)(Math.random()*listaPalabras.size())).getAdivinar();
+        for (int i = 0; i < adivinar.length() ; i++) {
+            System.out.print("_");
+        }
+    }
+
+}
---
+++ ArrayListEjemplo/src/main/java/org/example/Palabras.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Palabras {
+    private ArrayList<Palabra> lista;
+}
