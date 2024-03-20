6074de88f3cf16e46b53c97cbedf19b9e4b6092a
Gema Garcia <gema.garciafernandez@educa.madrid.org>
2024-03-20 12:24:43 +0100

    Ficheros

--- AhorcadoCapas/src/main/java/dao/DaoPalabrasFicheros.java
+++ AhorcadoCapas/src/main/java/dao/DaoPalabrasFicheros.java
@@ -11,22 +11,26 @@ import java.util.Scanner;
 public class DaoPalabrasFicheros {
     public static final String FICHERO = "Fichero";
     public static final String FICHEROB = "FicheroBinario";
+
     public static void crearFicheros() throws IOException {
         File fichero1 = new File(FICHERO);
         File fichero2 = new File(FICHEROB);
         if (!fichero1.exists())
-                fichero1.createNewFile();
+            fichero1.createNewFile();
         if (!fichero2.exists())
             fichero2.createNewFile();
     }
+
     public static void crearFicheros(String nombreFichero) throws IOException {
         File fichero1 = new File(nombreFichero);
         if (!fichero1.exists())
             fichero1.createNewFile();
     }
+
     public static List<Palabra> leerFichero() throws IOException {
         return leerFichero(DaoPalabrasFicheros.FICHERO);
     }
+
     public static List<Palabra> leerFichero(String fichero) throws IOException {
         crearFicheros();
         ArrayList<Palabra> auxiliar = null;
@@ -41,7 +45,8 @@ public class DaoPalabrasFicheros {
                 } catch (CategoriaException e) {
                     System.out.println(e.getMessage());
                 }
-            };
+            }
+            ;
         } catch (FileNotFoundException ex) {
             java.util.logging.Logger.getLogger(DaoPalabrasFicheros.class.getName()).log(java.util.logging.Level.SEVERE, ex.getMessage(), ex);
 
@@ -51,7 +56,27 @@ public class DaoPalabrasFicheros {
 
     }
 
-    public static boolean escribirFichero (List<Palabra> lista, String nombreFichero) throws FileNotFoundException {
+    public static List<Palabra> leerFicheroBufferedReader(String fichero) throws IOException {
+        ArrayList<Palabra> auxiliar = null;
+        BufferedReader br = new BufferedReader(new FileReader(fichero));
+        auxiliar = new ArrayList<>();
+        String cadena;
+        do {
+            cadena = br.readLine();
+            if (cadena != null) {
+                String[] trozos = cadena.split(";");
+                try {
+                    //auxiliar.add(new Palabra(Integer.parseInt(trozos[0]),Integer.parseInt(trozos[1]),trozos[2],trozos[3]));
+                    auxiliar.add(new Palabra(cadena));
+                } catch (CategoriaException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        } while (cadena != null);
+    return auxiliar;
+}
+
+    public static boolean escribirFichero(List<Palabra> lista, String nombreFichero) throws FileNotFoundException {
         String cadena = null;
         PrintWriter pw = new PrintWriter(nombreFichero);
         //? si no está vacía
@@ -61,45 +86,49 @@ public class DaoPalabrasFicheros {
         pw.close();
         return true;
     }
+
     public static void escribirFicheroConsola(String nombreFichero) throws FileNotFoundException {
         Scanner lector = new Scanner(System.in);
         String cadena = null;
         PrintWriter pw = new PrintWriter(nombreFichero);
         System.out.println("Introduce las líneas que quieras escribir en el fichero, para acabar pon FIN");
-        do{
+        do {
             cadena = lector.nextLine();
             if (!cadena.equalsIgnoreCase("fin"))
                 pw.println(cadena);
-        }while (!cadena.equalsIgnoreCase("fin"));
+        } while (!cadena.equalsIgnoreCase("fin"));
         pw.close();
     }
-    public static void LeerFichero (String nombreFichero) throws FileNotFoundException {
+
+    public static void LeerFichero(String nombreFichero) throws FileNotFoundException {
         Scanner lector = new Scanner(new File(nombreFichero));
         String cadena;
-        while(lector.hasNextLine()){
+        while (lector.hasNextLine()) {
             System.out.println(lector.nextLine());
         }
         System.out.println("Se ha alcanzado el final del fichero");
     }
+
     public static void escribirFicheroConsolaNoSobreescribir(String nombreFichero) throws IOException {
         Scanner lector = new Scanner(System.in);
         String cadena = null;
         /*FileWriter fw = new FileWriter(nombreFichero,true);
         PrintWriter pw1 = new PrintWriter(fw);*/
-        PrintWriter pw = new PrintWriter(new FileWriter(nombreFichero,true));
+        PrintWriter pw = new PrintWriter(new FileWriter(nombreFichero, true));
 
         System.out.println("Introduce las líneas que quieras escribir en el fichero, para acabar pon FIN");
-        do{
+        do {
             cadena = lector.nextLine();
             if (!cadena.equalsIgnoreCase("fin"))
                 pw.println(cadena);
-        }while (!cadena.equalsIgnoreCase("fin"));
+        } while (!cadena.equalsIgnoreCase("fin"));
         pw.close();
     }
 
     /**
      * Ejemplo de lectura de fichero binario. Pensad cómo utilizarlo para guardar y recuperar partida, guardando el objeto juego
      * en vez del ArrayList
+     *
      * @return
      */
     public static List<Palabra> leerFicheroBinario() {
@@ -127,4 +156,4 @@ public class DaoPalabrasFicheros {
     }
 
 
-  }
+}
